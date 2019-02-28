ed38b3ab6de6c085599bae2218429a15c40e472b
Paul Norman <penorman@mac.com>
2019-02-27 22:37:37 -0800

    Rename the function used for pixel areas

--- vector/functions.sql
+++ vector/functions.sql
@@ -1,7 +1,6 @@
-CREATE OR REPLACE FUNCTION pixel_size (z integer)
+CREATE OR REPLACE FUNCTION pixel_area (z integer)
   RETURNS float
   AS 'SELECT (24505721471.3958/(2^(2*z)));'
   LANGUAGE SQL
   IMMUTABLE
   STRICT;
-
